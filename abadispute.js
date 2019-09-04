e87eb8ced8fcafd98c7ff3947f725213fc29eced
Moritz <moe-@gmx.at>
2019-09-05 00:35:37 +0200

    getDerivation

--- abadispute.js
+++ abadispute.js
@@ -1,34 +1,103 @@
-const { Map, Set } = require('immutable');
+const { Map, Set, fromJS } = require('immutable');
+
+fCompute = t => n => {
+    if (n > t.step && !t.get('aborted') && !t.get('success')) {
+        if (t.get('children').size == 0) {
+            t = fAlgorithmStep(t);
+        }
+        
+        t.set(
+            'children', 
+            t.get('children').map(
+                tChild => fCompute(n, tChild)
+            )
+        );
+    }
+
+    return t;
+}
+
+fAlgorithmStep = t => {
+    let success = false; //@todo
+    let aborted = false; //@todo
+
+    if (success) t = t.set('success', true);
+    if (aborted) t = t.set('aborted', true);
+    if (success || aborted) return t;
+    
+    //...
+
+    t.set(
+        'children', 
+        t.get('children').map(
+            tChild => 
+                tChild
+                    .set('step', t.step + 1)
+                    .set('children', Set([]))
+                    .set('aborted', false)
+                    .set('success', false)
+        );
+
+    return t;
+}
+
+fGetInitialT = (framework, sentence) => 
+    Map({
+        P: Set(),
+        O: Set(), // Set of Sets
+        D: Set(),
+        C: Set(),
+        F: Set(),
+        step: 0,
+        children: Set(), // Set of Tuples
+        aborted: false,
+        success: false,
+        path: List()
+    });
+
+fGetBranches = tList => {
+    let branches = List();
+
+    tList.map(
+        t => {
+            if (t.get('children').size = 0) {
+                t = branches.add(t);
+            } else {
+                t = branches.concat(fGetBranches(t.get('children')));
+            }
+        }
+    );
+
+    return branches;
+}
+
+fGetDerivation = (t, path, partialDerivation) => 
+    (path.size == 0) ?
+        partialDerivation.add(t) :
+        fGetDerivation(
+            t.getIn('children', path.first()), 
+            path.shift(),
+            partialDerivation.add(t)
+        );
 
 module.exports = {
     abadispute: (filters, updt, implementation) => 
         (framework, sentence) => {
-            let computationCounter = 0;
-            let branches = Map({});
+            let t = fGetInitialT(framework, sentence);
+
+            let getBranches = () => fGetBranches(List([t]));
+            let getPath = branch => getBranches().get(branch).get('path');
 
             return {
-                compute: (n) => {
-                    while (computationCounter < n) {
-                        console.log("computationCounter: " + computationCounter);
-                        computationCounter++;
-                    }
-
-                    if (branches.size == 0) {
-                        branches = branches.set(0, "first branch");
-                    }
-                },
-
-                getBranches: () => {
-                    return branches;
-                },
-
-                getSupport: (branch) => {
-                    return framework + "|" + sentence + "|" + branch + ": the support";
-                },
-
-                getDerivation: (branch) => {
-                    return framework + "|" + sentence + "|" + branch + ": the derivation";
-                }
+                compute: fCompute(t),
+                getBranches: fGetBranches(List([t])),
+                getSupport: branch => getBranches().get(branch).get('D'),
+                getDerivation: branch => 
+                    fGetDerivation(
+                        t, 
+                        getPath(branch),
+                        List()
+                    )
             }
     },
 
